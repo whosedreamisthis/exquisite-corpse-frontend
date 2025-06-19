6676da2e0687db0c573fc5687d74f899844cb8ed
Dana Sharon <whosedreamisthis@gmail.com>
2025-06-19 11:43:08 -0700

    still debugging

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
 	output: 'export', // Ensures a static export
 	basePath: '/exquisite-corpse-frontend-v2', // <--- IMPORTANT: This is your repository name
-	// assetPrefix: '/exquisite-corpse-frontend-v2/', // <--- IMPORTANT: Also needed for assets
+	assetPrefix: '/exquisite-corpse-frontend-v2/', // <--- IMPORTANT: Also needed for assets
 	images: {
 		unoptimized: true, // Optional: Improves static export compatibility for images
 	},
