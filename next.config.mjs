cb46501a67031bdfcf041da57ddbccf465bb0502
Dana Sharon <whosedreamisthis@gmail.com>
2025-06-19 12:14:57 -0700

    rename repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export', // Ensures a static export
-	basePath: '/exquisite-corpse-frontend-v2', // <--- IMPORTANT: This is your repository name
-	assetPrefix: '/exquisite-corpse-frontend-v2/', // <--- IMPORTANT: Also needed for assets
+	basePath: '/exquisite-corpse-frontend', // <--- IMPORTANT: This is your repository name
+	assetPrefix: '/exquisite-corpse-frontend/', // <--- IMPORTANT: Also needed for assets
 	images: {
 		unoptimized: true, // Optional: Improves static export compatibility for images
 	},
